ff3c697c4ad7947cca7ec3aec73e87a983cc03ee
Toru Miki <waviaei@gmail.com>
2017-09-14 01:28:17 +0900

    Implemented sass and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
-gulp.task('sass', function() {
-  return gulp.src('./sass/*.scss')
+gulp.task('precss', function() {
+  return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('./src/'))
+});
+
+gulp.task('postcss', function() {
+  return gulp.src('./src/*.css')
+  .pipe(postcss([
+    require('autoprefixer')({ browsers: ['last 1 versions'] }),
+    require('cssnano')
+  ]))
+  .pipe(gulp.dest('.'))
 });
